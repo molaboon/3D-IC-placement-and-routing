4b8baca800e96c24527390d0d8086cf07939cbc7
molaboon <how88889@gmail.com>
2024-10-05 10:36:31 +0800

    100 cell data

--- python_file/create_data.py
+++ python_file/create_data.py
@@ -1,18 +1,18 @@
 import random
 
-file = open("case1.1.txt", "a")
+file = open("100cell_case.txt", "a")
 
 cell = 100
 
-for i in range(cell):
-    tech = random.randint(1, 217)
-    file.write("Inst C" + str(i+1) + " MC" + str(tech) + "\n")
+# for i in range(cell):
+#     tech = random.randint(1, 217)
+#     file.write("Inst C" + str(i+1) + " MC" + str(tech) + "\n")
 
 for j in range(cell):
-    pin = random.randint(1, 3)
-    file.write("Net N" + str(j) + " " + str(pin)+"\n")
+    pin = random.randint(2, 3)
+    file.write("Net N" + str(j+1) + " " + str(pin)+"\n")
     
     for p in range(pin):
-        c = random.randint(1, 217)
+        c = random.randint(1, 100)
         file.write("Pin C" + str(c) + "/P1\n")
 
